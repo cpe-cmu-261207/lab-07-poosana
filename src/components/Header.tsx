c8459d25c112a78ce03a7287acfffba450841570
poosana <piixd1@gmail.com>
2021-09-05 16:02:31 +0700

    done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
-const Header = () => {
+  const Header = () => {
   return (
     <div>
       <p className='text-center text-3xl italic my-2'>Pixel Painter</p>
-      <p className='text-center'>by Chayanin Suatap 610631100</p>
+      <p className='text-center'>by Poosana Thassanavisut 620612161</p>
     </div>
   )
 }
--- src/stores/PixelPainterStore.ts
+++ src/stores/PixelPainterStore.ts
@@ -1,22 +1,74 @@
-import {Store} from 'pullstate'
+import { Store } from 'pullstate'
+import SelectColorButton from '../components/SelectColorButton'
 
 type PixelPainterStoreType = {
   //we save painted color as hex code (string) in 2D array
-  canvas: string[][] 
+  canvas: string[][]
+  currcolor: string
 }
 
 //return an (16 x 16) 2D array filled with "#FFFFFF"
 const createEmptyCanvas = () => {
   const output: string[][] = []
-  for (let i=0; i<16; i++){
+  for (let i = 0; i < 16; i++) {
     output[i] = []
-    for (let j=0; j<16; j++){
+    for (let j = 0; j < 16; j++) {
       output[i].push('#FFFFFF')
     }
   }
   return output
 }
 
+const setrandomColor = () => {
+  const output: string[][] = []
+  const color: string[] = ['#000000', '#804000', '#FE0000','#FE6A00', '#FFD800', '#00FF01', '#FFFFFF', '#01FFFF', '#0094FE', '#0026FF' , '#B100FE', '#FF006E']
+  
+  for (let i = 0; i < 16; i++) {
+    output[i] = []
+    for (let j = 0; j < 16; j++) {
+      const x = Math.floor(Math.random()*12)
+      output[i].push(color[x])
+    }
+  }
+  return output
+}
+
+export const randomColor = () => {
+  PixelPainterStore.update(
+    s => { s.canvas = setrandomColor() }
+  )
+}
+
+export const selColor = (color: string) => {
+  PixelPainterStore.update(
+    s => { s.currcolor = color}
+  )
+}
+
+const setEmpty = () => {
+  const output: string[][] = []
+  for (let i = 0; i < 16; i++) {
+    output[i] = []
+    for (let j = 0; j < 16; j++) {
+      output[i].push('#FFFFFF')
+    }
+  }
+  return output
+}
+
+export const emptyCanvas = () => {
+  PixelPainterStore.update(
+    s => { s.canvas = setEmpty() }
+  )
+}
+
+export const setCell = (y:number , x:number) => {
+  PixelPainterStore.update(
+    s => { s.canvas[y][x] = s.currcolor}
+  )
+}
+
 export const PixelPainterStore = new Store<PixelPainterStoreType>({
-  canvas: createEmptyCanvas()
+  canvas: createEmptyCanvas(),
+  currcolor: ""
 })
